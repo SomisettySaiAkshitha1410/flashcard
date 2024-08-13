a71d06271ca7782ff3cc93371eb1ccd2925823ca
Akshitha <akshithasomisetty@gmail.com>
2024-08-13 16:56:15 +0530

    change in mysql port

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,7 @@ const connection = mysql.createConnection({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
+  port:3306
 });
 
 connection.connect((err) => {
--- backend/sync.js
+++
@@ -1,9 +0,0 @@
-// backend/sync.js
-const sequelize = require('./config/database');
-const Flashcard = require('./models/flashcard');
-
-sequelize.sync({ force: true }).then(() => {
-  console.log('Database & tables created!');
-}).catch(err => {
-  console.error('Unable to create table:', err);
-});
